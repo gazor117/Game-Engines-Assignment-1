5b9dd88bd5551b62c45f38f7e6fc0d680f23b8a3
gazor117 <oreillygary7@gmail.com>
2018-12-19 14:56:13 +0000

    Sphere Rotating

--- Game Engines Assignment 1/Assets/FreqBandCube.cs
+++ Game Engines Assignment 1/Assets/FreqBandCube.cs
@@ -21,11 +21,13 @@ public class FreqBandCube : MonoBehaviour
     {
         if (useBuffer == true)
         {
-            transform.localScale = new Vector3 (transform.localScale.x, (AudioAnalise.bandBuffer[bandIndex] * scaleMultiplier) + startscale, transform.localScale.z);
+            transform.localScale = new Vector3 ((AudioAnalise.bandBuffer[bandIndex] * scaleMultiplier) + startscale, (AudioAnalise.bandBuffer[bandIndex] * scaleMultiplier) + startscale, 
+                (AudioAnalise.bandBuffer[bandIndex] * scaleMultiplier) + startscale);
         }
         if (useBuffer == false)
         {
-            transform.localScale = new Vector3 (transform.localScale.x, (AudioAnalise.freqBands[bandIndex] * scaleMultiplier) + startscale, transform.localScale.z);
+            transform.localScale = new Vector3 ((AudioAnalise.freqBands[bandIndex] * scaleMultiplier) + startscale, (AudioAnalise.freqBands[bandIndex] * scaleMultiplier) + startscale, 
+                (AudioAnalise.freqBands[bandIndex] * scaleMultiplier) + startscale);
         }
         
       
---
+++ Game Engines Assignment 1/Assets/RotateSphere.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSphere : MonoBehaviour
+{
+    public float VerticalRotateSpeed;
+    public float HorizontalRotateSpeed;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right * (VerticalRotateSpeed * Time.deltaTime));
+        
+        transform.Rotate(Vector3.up * (HorizontalRotateSpeed * Time.deltaTime));
+
+    }
+}
